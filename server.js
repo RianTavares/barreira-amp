fb659b2a404c7b218f8054bf55c0a8d6807cf1c0
riantavares <rian.tavares@live.com>
2020-06-02 16:19:10 -0300

    criando servidor de estáticos para servir a o projeto

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/views/index.html'));
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ', + port);
